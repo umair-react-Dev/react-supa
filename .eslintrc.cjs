ddcf695eeac621845cd89797129385790fd6f684
umair abbas <umairabbas@bearplex.com>
2024-01-11 14:44:14 +0500

    eslint updated

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,21 @@
-// module.exports = {
-//   root: true,
-//   env: { browser: true, es2020: true, node: true },
-//   extends: [
-//     "eslint:recommended",
-//     "plugin:@typescript-eslint/recommended",
-//     "plugin:react-hooks/recommended",
-//   ],
-//   ignorePatterns: ["dist", ".eslintrc.cjs"],
-//   parser: "@typescript-eslint/parser",
-//   plugins: ["react-refresh"],
-//   rules: {
-//     "react-refresh/only-export-components": [
-//       "warn",
-//       { allowConstantExport: true },
-//     ],
-//   },
-// };
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true, node: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:react-hooks/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    // "plugin:@typescript-eslint/stylistic-type-checked",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+  },
+};
